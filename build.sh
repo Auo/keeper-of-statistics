00e7952d82ffffb7b01c6a070f5507a7053d7412
Andreas Lindgren <andreas.lindgren@outlook.com>
2018-11-16 12:54:17 +0100

    build should ouput path

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ rm -rf dist &&
 mkdir -p ./dist/KeeperOfStatistics/ &&
 cp *.lua ./dist/KeeperOfStatistics/ &&
 cp *.toc ./dist/KeeperOfStatistics/ &&
-zip -r ./dist/KeeperOfStatistics.zip ./dist/KeeperOfStatistics
+zip -r ./dist/KeeperOfStatistics.zip ./dist/KeeperOfStatistics &&
+echo 'created file: '$(pwd -P ./dist/KeeperOfStatistics.zip)/KeeperOfStatistics.zip
