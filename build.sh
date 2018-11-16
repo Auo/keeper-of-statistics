cc5bd445839528863a8ac579907276d05519be01
auo <1984096+Auo@users.noreply.github.com>
2018-11-16 20:02:55 +0100

    output correct path

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ mkdir -p ./dist/KeeperOfStatistics/ &&
 cp *.lua ./dist/KeeperOfStatistics/ &&
 cp *.toc ./dist/KeeperOfStatistics/ &&
 zip -r ./dist/KeeperOfStatistics.zip ./dist/KeeperOfStatistics &&
-echo 'created file: '$(pwd -P ./dist/KeeperOfStatistics.zip)/KeeperOfStatistics.zip
+echo 'created file: '$(pwd -P ./dist/KeeperOfStatistics.zip)/dist/KeeperOfStatistics.zip
