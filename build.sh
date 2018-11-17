a88cd4c1a1763403bfbb03de889ee93c652acf7f
auo <1984096+Auo@users.noreply.github.com>
2018-11-17 09:55:50 +0100

    zip sorted

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ rm -rf dist &&
 mkdir -p ./dist/KeeperOfStatistics/ &&
 cp *.lua ./dist/KeeperOfStatistics/ &&
 cp *.toc ./dist/KeeperOfStatistics/ &&
-zip -r ./dist/KeeperOfStatistics.zip ./dist/KeeperOfStatistics &&
+cd dist &&
+zip -r ./KeeperOfStatistics.zip ./KeeperOfStatistics &&
 echo 'created file: '$(pwd -P ./dist/KeeperOfStatistics.zip)/dist/KeeperOfStatistics.zip
